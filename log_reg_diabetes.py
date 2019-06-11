3e1904a32657a11496729399a533ce31645dcab9
LutsenkoAlexey <48469791+LutsenkoAlexey@users.noreply.github.com>
2019-06-11 14:42:49 +1000

    Add files via upload

---
+++ log_reg_diabetes.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from cross_validation_plotter import plot_cross_validation
+from sklearn.model_selection import train_test_split
+plt.style.use('ggplot')
+
+#Load the dataset
+df = pd.read_csv("diabetes.csv")
+
+#Print the first 5 rows of the dataframe.
+df.head()
+df.shape
+
+#создадим пустые массивы для объектов и цели
+X = df.drop('Outcome',axis=1).values
+y = df['Outcome'].values
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.4,random_state=42, stratify=y)
+
+#Setup для хранения точности обучения и тестирования
+neighbors = np.arange(1,9)
+train_accuracy =np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+clf = LogisticRegression(random_state=123, solver='lbfgs',
+                         multi_class='multinomial',  max_iter=300)
+
+clf.fit(X, y)
+
+print("have diabetes or not?")
+n = 1
+print(clf.predict(X[14:23]))
+print((X[14:23]))
+
+# Забираем функцию для построения графиков (не забываем подгрузить cross_validation_plotter)
+print("---------------------------------------")
+help(plot_cross_validation) # 
+print("---------------------------------------")
+print("Вызов функции")
+plot_cross_validation(X=X, y=y, clf=clf, title="Logistic Regression")
+print("---------------------------------------")
