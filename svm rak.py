e22f65a754974a2b501606cee243b0c0e4cdf979
LutsenkoAlexey <48469791+LutsenkoAlexey@users.noreply.github.com>
2019-06-11 18:51:40 +1000

    Add files via upload

---
+++ svm rak.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from cross_validation_plotter import plot_cross_validation
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn import svm
+
+data = pd.read_csv("wdbc.data", sep = ",")
+data = data.iloc[:,0:12]
+#print(data.describe())
+X = data.iloc[:,2:12]
+y = data.iloc[:,1]
+
+# Забираем рабочий код у ирисов
+
+C = 1.0 # параметр регуляризации SVM
+svc = svm.SVC(kernel='linear', C=1, gamma=0.1)
+svc.fit(X, y)
+
+print("Where is malignant cancer?")
+n = 1
+print(svc.predict(X[14:23]))
+print((X[14:23]))
+
+# Забираем функцию для построения графиков (не забываем подгрузить cross_validation_plotter)
+print("---------------------------------------")
+help(plot_cross_validation) # 
+print("---------------------------------------")
+print("Вызов функции")
+plot_cross_validation(X=X, y=y, clf=svc, title="SVM")
+print("---------------------------------------")
