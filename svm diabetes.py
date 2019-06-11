900563a4d6e287a1e48353653928bc6cb8063baa
LutsenkoAlexey <48469791+LutsenkoAlexey@users.noreply.github.com>
2019-06-11 14:50:13 +1000

    Add files via upload

---
+++ svm diabetes.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from cross_validation_plotter import plot_cross_validation
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn import svm
+
+#Load the dataset
+df = pd.read_csv("diabetes.csv")
+
+#Print the first 5 rows of the dataframe.
+df.head()
+df.shape
+
+#создадим пустые массивы для объектов и цели
+X = df.drop('Outcome',axis=1).values
+y = df['Outcome'].values
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.4,random_state=42, stratify=y)
+
+#Setup для хранения точности обучения и тестирования
+neighbors = np.arange(1,9)
+train_accuracy =np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+C = 1.0 # параметр регуляризации SVM
+svc = svm.SVC(kernel='linear', C=1, gamma=0.1)
+svc.fit(X, y)
+
+print("have diabetes or not?")
+n = 1
+print(svc.predict(X[14:23]))
+print((X[14:23]))
+
+# Забираем функцию для построения графиков (не забываем подгрузить cross_validation_plotter)
+print("---------------------------------------")
+help(plot_cross_validation) # 
+print("---------------------------------------")
+print("Вызов функции")
+plot_cross_validation(X=X, y=y, clf=svc, title="SVM")
+print("---------------------------------------")
